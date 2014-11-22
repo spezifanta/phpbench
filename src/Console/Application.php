e23a273f51f5642baf2247ae1cc2d98e710eaa28
Alexander Kuhrt <alexander.kuhrt@reservix.de>
2014-11-22 22:42:23 +0100

    adds cons VERSION and sets memoery_limit to unlimited

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -6,12 +6,14 @@ use Bench\Console\Command\BenchMarkCommand;
 
 class Application extends \Symfony\Component\Console\Application
 {
+    const VERSION = 0.1;
+
     public function __construct()
     {
         error_reporting(-1);
-        ini_set('memory_limit', '2G');
+        ini_set('memory_limit', '-1');
 
-        parent::__construct('PHP Benchmark', 0.1);
+        parent::__construct('PHP Benchmark', self::VERSION);
 
         $this->add(new BenchMarkCommand());
     }
